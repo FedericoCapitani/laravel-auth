afc91202419100fb5c52ac45be3db69baa71c4df
FedericoCapitani <federico.capitani@outlook.it>
2022-06-22 10:06:15 +0200

    add missing files

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,6 @@
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <h1>Work in Progress</h1>
-    </div>
-</div>
+@extends('layouts.app')
+
+
+@section('app')
+<div id="app"></div>
 @endsection
